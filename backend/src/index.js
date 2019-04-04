d72d98f86902027ca82d2c19561657448e892e34
Erislandio Soares <erislandiosoares21@gmail.com>
2019-04-04 19:45:25 -0300

    adição da dependencia body parser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const PORT = 3001;
 const app = express();
 
+app.use(bodyParser.json());
 app.use(require("./router"));
 
 app.listen(PORT, () => {
