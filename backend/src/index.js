a8d3d4b9dcb5213f27e051d64e69c7609a7fb4db
Erislandio Soares <erislandiosoares21@gmail.com>
2019-04-07 22:29:49 -0300

    adiçã do sockeio

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,15 @@ const bodyParser = require("body-parser");
 const PORT = 3001;
 const app = express();
 
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
 app.use(bodyParser.json());
 app.use(require("./router"));
 
--- backend/src/router.js
+++ backend/src/router.js
@@ -3,8 +3,10 @@ const express = require("express");
 const routes = express.Router();
 
 const TweetControllers = require("./controllers/tweetControllers");
+const LinkControllers = require("./controllers/LikeControllers");
 
 routes.get("/tweets", TweetControllers.index);
 routes.post("/tweet", TweetControllers.store);
+routes.post("/likes/:id", LinkControllers.store);
 
 module.exports = routes;
