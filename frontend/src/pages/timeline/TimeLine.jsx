95503a4d7e281985c25cc843422331c4e694a2e2
Erislândio Soares <erislandio.soares@acct.global>
2019-06-10 22:18:25 -0300

    add template login

--- frontend/src/pages/timeline/TimeLine.jsx
+++ frontend/src/pages/timeline/TimeLine.jsx
@@ -1,10 +1,48 @@
 import React, { Component } from 'react'
+import { Logo } from '../../assets/icons/Logo';
+import './timeline.css'
+import api from '../../services/service'
+
 
 export default class TimeLine extends Component {
+
+
+    state = {
+        newTweet: ''
+    }
+
+    handleChangeInputValue = e => {
+        this.setState({ newTweet: e.target.value })
+    }
+
+    handleNewTweet = async e => {
+        if (e.keyCode !== 13) return;
+
+        const content = this.state.newTweet
+        const author = localStorage.getItem('username')
+
+        await api.post('tweets', {
+            content, author
+        })
+
+        this.setState({ newTweet: '' })
+
+    }
+
     render() {
         return (
-            <div>
-                Time line
+            <div className="timeline-wrapper">
+                <Logo color="#fff" />
+                <form>
+                    <textarea
+                        value={this.state.newTweet}
+                        onChange={e => this.handleChangeInputValue(e)}
+                        onKeyDown={this.handleNewTweet}
+                        placeholder="O que está acontecendo" />
+                </form>
+                <ul>
+
+                </ul>
             </div>
         )
     }
